6744d7bcc0a02b4a16e51acb10e4dd0652f3455d
Jess <maochengjie1988@gmail.com>
2016-10-31 16:57:41 +0800

    修改 基金列表页单个条目 样式

--- we-mobile/src/common-ui/fund-list-item/fund-list-item.js
+++ we-mobile/src/common-ui/fund-list-item/fund-list-item.js
@@ -51,7 +51,14 @@ $.extend( FundListItem.prototype, {
 
         this.fundData = data;
 
-        let fundName = data.fundName || '';
+        //是否是  货币基金
+        let isMoneyFund = false;
+
+        let fundType = data.fundType;
+
+        isMoneyFund = fundType === '4' || fundType === 'FUND_MONEY';
+
+        let fundName = data.abbrev || data.fundName || '';
         if( fundName.length > 6 ){
             fundName = fundName.substr(0, 6) + '...';
         }
@@ -66,29 +73,69 @@ $.extend( FundListItem.prototype, {
             tag = data.remark || '';
         }
 
+        let extraClass = '';
+
         let rateYearCon = '--';
-        if( data.rateYear ){
-            rateYearCon = `${data.rateYear}<span class="year-rise-span">%</span>`;
-        }
-
         let navCon = '--';
-        if( data.nav ){
-            navCon = `${data.nav}<span class="year-net-span"></span>`;
+        let label1 = '年涨跌幅';
+        let label2 = '净值(元)';
+        let isNewFund = true;
+
+
+        if( isMoneyFund ){
+            label1 = '七日年化收益率';
+            label2 = '万份收益(元)';
+
+            if( data.rate7day ){
+                isNewFund = false;
+                rateYearCon = `${data.rate7day}<span class="year-rise-span">%</span>`;
+
+                let rate7day = parseFloat( data.rate7day );
+                if( rate7day < 0 ){
+                    extraClass = 'fund-item-bad';
+                }
+            }
+
+            if( data.profit10k ){
+                isNewFund = false;
+                navCon = `${data.profit10k}<span class="year-net-span"></span>`;
+            }
+
+
+
+        }else{
+
+            if( data.rateYear ){
+                isNewFund = false;
+                rateYearCon = `${data.rateYear}<span class="year-rise-span">%</span>`;
+                
+                let rateYear = parseFloat( data.rateYear );
+                if( rateYear < 0 ){
+                    extraClass = 'fund-item-bad';
+                }
+            }
+            if( data.nav ){
+                isNewFund = false;
+                navCon = `${data.nav}<span class="year-net-span"></span>`;
+            }
+            
         }
 
+        let btnText = isNewFund ? '认购' : '申购';
 
-        let html = `<h1 class="fund-name">${fundName}<span class="fund-code">（${data.fundCode}）</span><span class="fund-span">${ tag }</span></h1>
-                <ul class="fund-content fn-clear">
+
+        let html = `<h1 class="fund-name ">${fundName}<span class="fund-code">（${data.fundCode}）</span><span class="fund-span">${ tag }</span></h1>
+                <ul class="fund-content fn-clear ${extraClass}">
                     <li class="fund-info info1">
                         <p class="year-rise">${ rateYearCon }</p>
-                        <p class="rise-text">年涨跌幅</p>
+                        <p class="rise-text">${ label1 }</p>
                     </li>
                     <li class="fund-info info2">
                         <p class="year-net">${ navCon }</p>
-                        <p class="rise-text">净值(元)</p>
+                        <p class="rise-text">${ label2 }</p>
                     </li>
                 </ul>
-                <div class="fund-buy-btn" id="gio-trick-${data.fundCode}">申购</div>
+                <div class="fund-buy-btn" id="gio-trick-${data.fundCode}">${btnText}</div>
                 `;
 
         this.$el.html( html ).addClass('glpb-fund-sug-theme-3').show();
